879ebc1e06b7379424e35e55c3764f2c9fd951dd
gicudobrin <mihaiclaudiu87@yahoo.com>
2018-02-28 15:19:57 +0200

    Remove some files from the repository

--- theDom/app.js
+++
@@ -1,7 +0,0 @@
-const myHeading = document.getElementById('myHeading');
-const myButton = document.getElementById('myButton');
-myButton.addEventListener('click', () => {
-	myHeading.style.color='red';
-});
-
-
