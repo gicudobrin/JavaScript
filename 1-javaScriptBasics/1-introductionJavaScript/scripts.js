240ad57a296a2e12e12591b9f61a37575971cde7
gicudobrin <mihaiclaudiu87@yahoo.com>
2018-03-01 14:34:43 +0200

    Start learning JavaScript

---
+++ 1-javaScriptBasics/1-introductionJavaScript/scripts.js
@@ -0,0 +1,4 @@
+alert('Hello from Treehouse');
+document.write('<h1>Welcome to JavaScript Basics');
+alert('Thanks for visiting.');
+console.log('Program complete');
---
+++ 1-javaScriptBasics/2-storingAndTrackingInformationWithVariables/scripts.js
@@ -0,0 +1,29 @@
+// var message = "Hello!";
+// alert(message);
+// message = "Welcome to JavaScript Basics";
+// alert(message);
+
+// var visitorName = prompt('What is your name?');
+// alert(visitorName);
+
+// var visitor = prompt('What is your name?');
+// var message = 'Hello' + ' ' + visitor;
+// document.write(message);
+
+// var firstName = 'Clau';
+// var lastName = 'Mihai';
+// var fullName = firstName + ' ' + lastName;
+// document.write(fullName);
+
+// var passphrase = 'Open Sesame';
+// document.write(passphrase.length);
+// var lowerCase = passphrase.toLowerCase();
+// document.write(lowerCase);
+
+// var nume = 'alphabet';
+// document.write(nume.toUpperCase());
+
+// var stringToShout = prompt('What shall I shout?');
+// var shout = stringToShout.toUpperCase();
+// shout += '!!!';
+// alert(shout);
