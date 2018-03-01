74b69199eb5617716db6c81cf6a89362011e8f57
gicudobrin <mihaiclaudiu87@yahoo.com>
2018-03-01 17:16:32 +0200

    Work to learn JavaScript

---
+++ 1-javaScriptBasics/2-storingAndTrackingInformationWithVariables/story/story.js
@@ -0,0 +1,6 @@
+var adjective = prompt('Please type an adjective');
+var verb = prompt('Please type a verb');
+var noun = prompt('Please type a noun');
+alert('All done. Ready for the message?');
+var sentence = "<h2>There once was a " + adjective + " programmer who wanted to use JavaScript to " + verb + ' the ' + noun + '.';
+document.write(sentence);
---
+++ 1-javaScriptBasics/3-workingWithNumbers/scripts.js
@@ -0,0 +1,33 @@
+// var secondsPerMinut = 60;
+// var minsPerHour = 60;
+// var hoursPerDay = 24;
+// var daysPerWeek = 7;
+// var weeksPerYear = 52;
+// var secondsPerDay = secondsPerMinut * minsPerHour * hoursPerDay;
+// document.write("There are " + secondsPerDay + " seconds in a day");
+
+// var questions = 3;
+// var questionsLeft = ' [' + questions + ' questions left]';
+// var adjective = prompt("Please type an adjective" + questionsLeft);
+// questions -= 1;
+// questionsLeft = ' [' + questions + ' questions left]';
+// var verb = prompt("Please type a verb" + questionsLeft);
+// questions -= 1;
+// questionsLeft = ' [' + questions + ' questions left]';
+// var noun = prompt("Please type a noun" + questionsLeft);
+// alert("All done! Ready for the message?");
+// var sentence = "<h1>There once was a " + adjective;
+// sentence += " programmer who wanted to use JavaScript to " + verb;
+// sentence += " the " + noun + ".</h1>";
+// document.write(sentence);
+
+// var dieRoll = Math.floor(Math.random() * 6);
+// alert("You rolled a " + dieRoll);
+
+// var input1 = "Please type a starting number";
+// var bottomNumber = parseInt(input1);
+// var input = prompt("Please type a number");
+// var topNumber = parseInt(input);
+// var randomNumber = Math.floor(Math.random() * topNumber) + 1;
+// var message = "<h1>" + randomNumber + " is a number between 1 and " + topNumber + ".</h1>";
+// document.write(message);
