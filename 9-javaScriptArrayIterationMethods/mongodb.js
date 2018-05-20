015072382d2f30fe9876198b5ce9550f5e43b296
gicudobrin <mihaiclaudiu87@yahoo.com>
2018-05-20 13:00:55 +0300

    Modify

--- 9-javaScriptArrayIterationMethods/mongodb.js
+++ 9-javaScriptArrayIterationMethods/mongodb.js
@@ -13,6 +13,6 @@ MongoClient.connect(url, function(err, db) {
 
 //import a json file in my atlas cluster
 
-mongoimport --uri "mongodb+srv://mihai:1234567a@gettingstarted-sszae.mongodb.net/m201" --collection people --type json --file people.json
+// mongoimport --uri "mongodb+srv://mihai:1234567a@gettingstarted-sszae.mongodb.net/m201" --collection people --type json --file people.json
 
-mongoimport --db users --collection contacts --type csv --headerline --file /opt/backups/contacts.csv
+// mongoimport --db users --collection contacts --type csv --headerline --file /opt/backups/contacts.csv
